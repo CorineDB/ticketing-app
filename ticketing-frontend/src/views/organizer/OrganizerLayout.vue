6b093a0cf6e36c57ff92a81376595d7726832990
HÔKWÈ Holding <hokweholding@gmail.com>
2025-11-26 15:34:25 +0100

    commit

--- ticketing-frontend/src/views/organizer/OrganizerLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <router-view></router-view>
-  </div>
-</template>
-
-<script setup lang="ts">
-// This component acts as a layout for the nested organizer routes.
-</script>
