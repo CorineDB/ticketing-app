ea3701691616ab73bf80b6b2ae5d3f77307fe8e7
HÔKWÈ Holding <hokweholding@gmail.com>
2025-11-27 08:34:48 +0100

    commit

--- ticketing-api-rest-app/routes/api.php
+++ ticketing-api-rest-app/routes/api.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\Api\TicketTypeController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\WebhookController;
-use App\Http\Controllers\Api\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 // Auth routes
@@ -53,17 +52,11 @@ Route::post('/scan/confirm', [ScanController::class, 'confirm'])
 // Webhooks (public, no authentication)
 Route::post('/webhooks/fedapay', [WebhookController::class, 'fedapayWebhook']);
 
-// Payment callback (public, no authentication - FedaPay redirects users here)
-Route::get('/payment/callback', [PaymentController::class, 'callback'])->name('payment.callback');
-
-// Ticket purchase (public - no authentication required to buy tickets)
-Route::post('/tickets/purchase', [TicketController::class, 'purchase']);
-
 // Protected routes (authentication required)
 Route::middleware('auth:sanctum')->group(function () {
     // Roles
     Route::apiResource('roles', RoleController::class);
-    
+
     Route::apiResource('users', UserController::class);
     Route::post('/organizers', [UserController::class, 'storeOrganizer']);
 
