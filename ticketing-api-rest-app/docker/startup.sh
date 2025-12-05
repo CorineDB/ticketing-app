c4069f29bf039461d6eb7d511e90c4fdc2e3e6a7
Corine D. BOCOGA <cbocoga@celeriteholding.com>
2025-12-05 12:57:43 +0100

    Commit

--- ticketing-api-rest-app/docker/startup.sh
+++ ticketing-api-rest-app/docker/startup.sh
@@ -121,7 +121,7 @@ else
     # 5. Mise en cache pour la PROD
     echo "⚡ Mise en cache..."
     echo "⚡ Mise en cache de la configuration..."
-    php artisan config:clear
+    php artisan optimize:clear
     php artisan route:clear
     php artisan view:clear
     php artisan config:cache
